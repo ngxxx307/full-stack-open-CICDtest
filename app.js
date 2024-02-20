ab631081b73d9a367aa031539c4339f8e9273139
ngxxx307 <71307064+ngxxx307@users.noreply.github.com>
2024-02-20 20:34:16 +0800

    test

--- app.js
+++ app.js
@@ -14,3 +14,9 @@ app.listen(PORT, () => {
 app.get('/version', (req, res) => {
   res.send('2') // change this string to ensure a new version deployed
 })
+
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
+  res.send('ok')
+})
