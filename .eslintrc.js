8d43c0d78c100cb7e81b2a3d631344ba28c93898
ngxxx307 <71307064+ngxxx307@users.noreply.github.com>
2024-02-20 18:08:26 +0800

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,15 +22,11 @@ module.exports = {
   "rules": {
     "indent": [
       "error",
-        2
-      ],
+      2
+    ],
     "linebreak-style": [
       "error",
       "unix"
-    ],
-    "quotes": [
-      "error",
-      "double"
     ],
     "semi": [
       "error",
